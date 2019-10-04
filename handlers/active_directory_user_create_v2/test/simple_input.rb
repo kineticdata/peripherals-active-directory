fd51675df912f9b0dcf207fccb8a2367fe30d4b1
rehmc <chad.rehm@kineticdata.com>
2019-10-04 13:26:29 -0500

    PER-170 restored the simple_input files

---
+++ handlers/active_directory_user_create_v2/test/simple_input.rb
@@ -0,0 +1,25 @@
+{
+  'info' => {
+    'host' => "",
+    'username' => "",
+    'password' => "",
+    'port' => '',
+    'base' => '',
+    'dn_format' => '',
+    'enable_debug_logging' => 'Yes'
+  },
+  'parameters' => {
+    'user_logon' => 'kte.testuser@domain.com',
+    'first_name'=> 'KTE',
+    'last_name' => 'Test User',
+    'full_name' => 'KTE Test User',
+    'email' => 'kte.testuser@domain.com',
+    'description' => 'Test Account',
+    'office' => 'A2 4th Floor',
+    'telephone' => '555-555-5555',
+    'title' => 'Project Manager',
+    'department' => 'Processing and Exchange',
+    'company' => 'Kinetic Data',
+    'activated' => 'No'
+  }
+}
